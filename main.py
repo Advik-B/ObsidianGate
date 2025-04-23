529d5629b27f3fbd222fb07a41b3c73ac2cb51ac
Advik <advik.b@gmail.com>
2025-04-23 18:42:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-from launcher.services.runtime_downloader import get_available_runtimes
+from launcher.services.runtime_downloader import get_available_runtimes, download_jre
+
+
+x64 = get_available_runtimes().x64
+
+download_jre(x64, "jre")
